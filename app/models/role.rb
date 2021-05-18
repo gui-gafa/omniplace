2a1981f2c559cc394c1b1524edd7622c9cf8607a
Guilherme Pelusi <guilherme.pelusi@gmail.com>
2021-05-18 13:03:27 -0300

    dependent destroy on role models

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,6 @@
 class Role < ApplicationRecord
+  has_many :assignments, dependent: :destroy
+  has_many :users, through: :assignments
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
