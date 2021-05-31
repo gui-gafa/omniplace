52faf0961b256fb5bd94a2b99828edadfb6fdff2
Guilherme Pelusi <guilherme.pelusi@gmail.com>
2021-05-31 12:54:38 -0300

    melhorada na view dos loans

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -5,6 +5,9 @@ class LoansController < ApplicationController
   def index
     @loans = policy_scope(Loan).order(:date)
     @other_users = User.all_except(current_user)
+    @loans_taken = @loans.where(taker: current_user).sum(:value)
+    @loans_provided =  @loans.where(provider: current_user).sum(:value)
+    @saldo_total = @loans_provided - @loans_taken
   end
 
   # GET /loans/1
